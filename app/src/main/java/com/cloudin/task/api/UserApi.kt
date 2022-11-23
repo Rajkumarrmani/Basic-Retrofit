8e3555e2526f0fcd856bd65be2ded63159bab56e
Raj <m.rajuece91@gmail.com>
2022-11-23 09:28:08 +0530

    User Adapter completed

--- app/src/main/java/com/cloudin/task/api/UserApi.kt
+++ app/src/main/java/com/cloudin/task/api/UserApi.kt
@@ -1,6 +1,5 @@
 package com.cloudin.task.api
 
-import androidx.lifecycle.MutableLiveData
 import com.cloudin.task.data.model.User
 import retrofit2.Response
 import retrofit2.http.GET
--- app/src/main/java/com/cloudin/task/ui/home/MainActivity.kt
+++ app/src/main/java/com/cloudin/task/ui/home/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.cloudin.task.ui.home
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import com.cloudin.task.R
 import com.cloudin.task.databinding.ActivityMainBinding
 import com.cloudin.task.ui.adapter.UserAdapter
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,20 +22,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-        binding.recyclerView.adapter = userAdapter
 
+        binding.apply {
+            progressIndicator.visibility = View.VISIBLE
+            noUsers.visibility = View.GONE
+            recyclerView.visibility = View.GONE
+            recyclerView.adapter = userAdapter
+        }
 
         setUpObservers()
-
     }
 
     private fun setUpObservers() {
         viewModel.userData2.observe(this) { data ->
-            userAdapter.updateItems(data.body()?.results)
+            if (data.isSuccessful && data.code() == 200) {
+                binding.apply {
+                    progressIndicator.pauseAnimation()
+                    progressIndicator.visibility = View.GONE
+                    noUsers.visibility = View.GONE
+                    recyclerView.visibility = View.VISIBLE
+                }
+                userAdapter.updateItems(data.body()?.results)
+            } else {
+                binding.apply {
+                    progressIndicator.visibility = View.GONE
+                    recyclerView.visibility = View.GONE
+                    noUsers.visibility = View.GONE
+                }
+                Toast.makeText(this, resources.getText(R.string.server_error), Toast.LENGTH_SHORT)
+                    .show()
+            }
         }
-    }
-
-    companion object {
-        private const val TAG = "MainActivity"
     }
 }
